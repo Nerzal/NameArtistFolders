b131157b67ea6aa9ab06fc7627204a43c2963471
Nerzal <theel.tobias@gmx.de>
2017-08-12 13:24:55 +0200

    now Fetching artist folder paths while skipping first level

--- RenameArtistFolders/RenameArtistFolders/Program.cs
+++ RenameArtistFolders/RenameArtistFolders/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RenameArtistFolders {
-  class Program {
+  public class Program {
     static void Main(string[] args) {
+      Console.WriteLine("### RenameArtistFolders ###");
+      Console.WriteLine(@"Please insert the root path. (Like C:\foo\music)");
+      string input = Console.ReadLine();
+      Console.WriteLine("Checking directory..");
+      while (!Directory.Exists(input)) {
+        Console.WriteLine(@"Please insert the root path. (Like C:\foo\music)");
+        input = Console.ReadLine();
+      }
+      Console.WriteLine("Directory found..");
+      Manager manager = new Manager(input, true);
+      IEnumerable<string> artistFolders = manager.ScanFolder();
+    }
+  }
+
+  public class Manager {
+    private readonly string _rootPath;
+    private readonly bool _skipFirstLevel;
+
+    public Manager(string rootPath, bool skipFirstLevel) {
+      this._rootPath = rootPath;
+      this._skipFirstLevel = skipFirstLevel;
+    }
+
+    public IEnumerable<string> ScanFolder() {
+      List<string> result = new List<string>();
+      string[] directories = Directory.GetDirectories(this._rootPath);
+      if (this._skipFirstLevel) {
+        foreach (string directory in directories) {
+          string[] artistDirectories = Directory.GetDirectories(directory);
+          result.AddRange(artistDirectories);
+        }
+        return result;
+      } else {
+        throw new NotImplementedException("Not skipping the first level is not implemented yet");
+      }
     }
   }
 }
